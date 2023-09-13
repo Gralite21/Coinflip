40f70a4f45dc459992532d3b9f66e4bb20596bf2
Tim Lagier <tim.lagier33@gmail.com>
2023-09-13 15:22:08 +0200

    Finished

--- CoinFlip.js
+++ CoinFlip.js
@@ -1,14 +1,22 @@
 function playGame (guess){
     const randomNumber = (Math.random());
+    const head = document.getElementById("head")
+    const tail = document.getElementById("tail")
     let result = '';
     let winOrLose = '';
     if (randomNumber < 0.5) {
         result = 'Heads' ;
+        document.querySelector('.coinSide').innerHTML='HEADS';
+        head.classList.remove('invisible');
+        tail.classList.add('invisible');
     } else{
         result = 'Tails';
+        document.querySelector('.coinSide').innerHTML='Tails';
+        tail.classList.remove('invisible');
+        head.classList.add('invisible');
     } if (guess===result){
-        document.querySelector('.result').innerHTML='YOU WON'
+        document.querySelector('.result').innerHTML='YOU WON';
     } else{
-        document.querySelector('.result').innerHTML='YOU LOST'
+        document.querySelector('.result').innerHTML='YOU LOST';
     }
 }
